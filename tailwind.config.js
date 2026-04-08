b661c14dbae6768d38f64d38f4d9910e6b7ac6b2
N-meapp <developersnme@gmail.com>
2026-04-08 11:44:18 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./templates/**/*.html",
+    "./**/templates/**/*.html",
+    "./static/**/*.js",
+  ],
   theme: {
     extend: {},
   },
